410a2c702b94f4fce26f9688e481617500acbe3d
elif c <86270311+elif-c@users.noreply.github.com>
2023-05-04 20:52:04 +0300

    Initial commit

---
+++ console-programlama/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace console_programlama
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("isim");
+            string name = Console.ReadLine();
+            Console.WriteLine("soyisim");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + " " + surname);
+            Console.ReadKey();
+        }
+    }
+}
