d1a491a82fd1661a7ad0e9bfed3a782f0460881d
elif c <86270311+elif-c@users.noreply.github.com>
2023-05-09 18:15:57 +0300

    Switch-case project added

---
+++ switch-case/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+int month = DateTime.Now.Month;
+
+switch (month)
+{
+    case 1:
+        Console.WriteLine("January");
+        break;
+    case 2:
+        Console.WriteLine("February");
+        break;
+    case 5:
+        Console.WriteLine("May");
+        break;
+    default:
+        Console.WriteLine("Wrong input");
+    break;
+}
+
+switch (month)
+{
+    case 3:
+    case 4:
+    case 5:
+        Console.WriteLine("It's spring time!");
+        break;
+    break;
+}
