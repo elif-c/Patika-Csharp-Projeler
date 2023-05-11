429ee77ef3e30bae089732080da1f36f9b643900
elif c <86270311+elif-c@users.noreply.github.com>
2023-05-11 19:34:38 +0300

    While & foreach project added

---
+++ donguler-while-foreach/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Enter a number: ");
+
+int number = int.Parse(Console.ReadLine());
+int counter = 1;
+int total = 0;
+
+while (counter <= number)
+{
+    total += counter;
+    counter++;
+}
+Console.WriteLine($"the average is {total/number}");
+
+// char character = 'a';
+// while (character < 'z')
+// {
+//     Console.WriteLine(character);
+//     character++;
+// }
+
+Console.WriteLine("** Foreach Loop used to loop in arrays and collections **");
+string[] cars = {"Lamborghini", "Volkswagen", "Volvo"};
+foreach (string car in cars) // variable type depends on what's in the array
+{
+    Console.WriteLine(car);
+}
