550e71a175ee365dc7f2f17c590129081903fa54
elif c <86270311+elif-c@users.noreply.github.com>
2023-05-13 02:34:41 +0300

    metotlar overloading project added

---
+++ metotlar-overloading/Program.cs
@@ -0,0 +1,47 @@
+﻿// out parameters
+
+string number = "999";
+
+bool result = int.TryParse(number, out int outnumber);
+if (result)
+    Console.WriteLine("successful parse " + outnumber);
+else
+{
+    Console.WriteLine("unsuccessful parse");
+}
+
+Metotlar instance = new Metotlar();
+instance.Sum(4, 5, out int totalSum);
+Console.WriteLine(totalSum);
+
+// method overloading
+int expression = 999;
+instance.Print(Convert.ToString(expression));
+instance.Print(expression);
+
+// method signature
+// method name + parameter number + parameter
+instance.Print("hey", " there");
+
+class Metotlar
+{
+    public void Sum(int a, int b, out int sum)
+    {
+        sum = a + b;
+    }
+
+    public void Print(string data)
+    {
+        Console.WriteLine(data);
+    }
+
+    public void Print(int data)
+    {
+        Console.WriteLine(data);
+    }
+
+    public void Print(string data, string data2)
+    {
+        Console.WriteLine(data + data2);
+    }
+}
