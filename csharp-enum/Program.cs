5f9bf02da2561d5f203c8eed3061f0c4b306a498
elif c <86270311+elif-c@users.noreply.github.com>
2023-05-20 20:34:22 +0300

    struct-kavrami and csharp-enum projects added

---
+++ csharp-enum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+// used when we need to work with indexes
+// very common and increases readability of the code
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(Days.Sunday);
+        Console.WriteLine((int)Days.Thursday);
+        Console.WriteLine((int)Days.Saturday);
+
+        int temperature = 28;
+        if (temperature < (int)Weather.Normal)
+        {
+            Console.WriteLine("Weather too cold");
+        }
+        else if (temperature > (int)Weather.Hot) 
+        {
+            Console.WriteLine("Weather too hot");
+        }
+        else if (temperature > (int)Weather.Normal && temperature < (int)Weather.TooHot)
+        {
+            Console.WriteLine("Let's go outside!");
+        }
+    }
+}
+
+enum Weather
+{   
+    // kept as enum data type, has to be converted
+    Cold = 4,
+    Normal = 15,
+    Hot = 23,
+    TooHot = 30
+}
+
+enum Days
+{
+    Monday, // the Monday member of Days enum, 0 as index, not a string
+    Tuesday, // we can set the index as Monday=1
+    Wednesday,
+    Thursday,   // numerical data
+    Friday=23,
+    Saturday,
+    Sunday
+}
---
+++ struct-kavrami/Program.cs
@@ -0,0 +1,56 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Rectangle rectangle = new Rectangle();
+        // a class would set the fields to default values if
+        // no explicit value is given
+        // we could still call the method without setting the fields below
+        rectangle.x = 5;
+        rectangle.y = 8;
+
+        Console.WriteLine(rectangle.CalculateArea());
+
+        Rectangle_Struct rectangleStruct = new Rectangle_Struct();
+        rectangleStruct.x = 3;
+        rectangleStruct.y = 7;
+
+        Console.WriteLine(rectangleStruct.CalculateArea());
+
+        // we can create an object for a struct without the new keyword
+        Rectangle_Struct rectangleStruct2;
+        // a struct would not set default values to fields
+        // they have to be declared
+        rectangleStruct2.x = 6;
+        rectangleStruct2.y = 3;
+
+        Console.WriteLine(rectangleStruct2.CalculateArea());
+
+    }
+}
+
+// for retaining small amounts of data, using a struct is faster than a class
+
+class Rectangle
+{
+    public int x;
+    public int y;
+
+    public long CalculateArea()
+    {
+        return this.x * this.y;
+    }
+}
+
+struct Rectangle_Struct
+{
+    public int x;
+    public int y;
+
+    // we cannot define a constructor without parameters inside a struct
+
+    public long CalculateArea()
+    {
+        return this.x * this.y;
+    }
+}
